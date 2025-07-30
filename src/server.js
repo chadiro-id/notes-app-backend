316f38917db18fdc9a612b66884e026428917882
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-07-30 21:02:00 +0700

    Update notes service dependency from in memory into postgres

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const ClientError = require('./exceptions/ClientError');
 const notes = require('./api/notes');
-const NotesService = require('./services/inMemory/NotesService');
+const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
 const init = async () => {
