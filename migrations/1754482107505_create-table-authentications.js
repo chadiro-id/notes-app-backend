47a5ac9141498f4aaeb42aad9c482a163f29d3c4
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 19:09:42 +0700

    New: postgres migration scripts creating authentications table

---
+++ migrations/1754482107505_create-table-authentications.js
@@ -0,0 +1,13 @@
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {};
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.down = (pgm) => {};
