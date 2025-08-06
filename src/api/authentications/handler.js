fa0ed474cca91afc7be3486de46289af954c5a9d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 20:50:08 +0700

    Method for handling post auth

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -5,6 +5,29 @@ class AuthenticationsHandler {
     this._tokenManager = tokenManager;
     this._validator = validator;
   }
+
+  async postAuthenticationHandler(request, h) {
+    this._validator.validatePostAuthenticationPayload(request.payload);
+
+    const { username, password } = request.payload;
+    const id = await this._usersService.verifyUserCredential(username, password);
+
+    const accessToken = this._tokenManager.generateAccessToken({ id });
+    const refreshToken = this._tokenManager.generateRefreshToken({ id });
+
+    await this._authenticationsService.addRefreshToken(refreshToken);
+
+    const response = h.response({
+      status: 'success',
+      message: 'Authentication berhasil ditambahkan',
+      data: {
+        accessToken,
+        refreshToken,
+      },
+    });
+    response.code(201);
+    return response;
+  }
 }
 
 module.exports = AuthenticationsHandler;
