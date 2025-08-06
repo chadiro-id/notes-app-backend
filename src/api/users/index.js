2d6d44de481b24e3a89eaf00100060af17995360
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 15:59:54 +0700

    Configuring users plugin for hapi server

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,1 +1,11 @@
-// Hapi server plugin for users
+const UsersHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'users',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const usersHandler = new UsersHandler(service, validator);
+    server.route(routes(usersHandler));
+  },
+};
