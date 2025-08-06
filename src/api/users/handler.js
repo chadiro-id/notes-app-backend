1c8a9982867d6c1cb48fe9fe0fe5c59a311b2dc2
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 15:48:28 +0700

    Start creating Users handler script

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -1,1 +1,9 @@
 // Api handler for users.
+class UsersHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+  }
+}
+
+module.exports = UsersHandler;
