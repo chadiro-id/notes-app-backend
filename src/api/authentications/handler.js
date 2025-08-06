06a5d9741006c80e2a537fa15b1e938c5afe78ac
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 20:45:12 +0700

    Api plugins for authentications

---
+++ src/api/authentications/handler.js
@@ -0,0 +1,10 @@
+class AuthenticationsHandler {
+  constructor(authenticationsService, usersService, tokenManager, validator) {
+    this._authenticationsService = authenticationsService;
+    this._usersService = usersService;
+    this._tokenManager = tokenManager;
+    this._validator = validator;
+  }
+}
+
+module.exports = AuthenticationsHandler;
