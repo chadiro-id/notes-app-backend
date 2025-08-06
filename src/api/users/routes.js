bff43f02338d9ce4fd993b057d240dde706ce017
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-06 15:46:15 +0700

    Configuring api routes for users services

--- src/api/users/routes.js
+++ src/api/users/routes.js
@@ -1,1 +1,14 @@
-// Api routes for Users
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/users',
+    handler: handler.postUserHandler,
+  },
+  {
+    method: 'GET',
+    path: '/users/{id}',
+    handler: handler.getUserByIdHandler,
+  },
+];
+
+module.exports = routes;
