f28c910fc666bd8f86c61b89c813ce914ea05aed
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-07 16:25:36 +0700

    Api plugins notes collaborations

---
+++ src/api/collaborations/handler.js
@@ -0,0 +1,9 @@
+class CollaborationsHandler {
+  constructor(collaborationsService, notesService, validator) {
+    this._collaborationsService = collaborationsService;
+    this._notesService = notesService;
+    this._validator = validator;
+  }
+}
+
+module.exports = CollaborationsHandler;
