cf95f4eb8af3098058e669780ed830a3c84f88a5
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-13 15:11:24 +0700

    Configure instance logic

--- src/services/storage/StorageService.js
+++ src/services/storage/StorageService.js
@@ -1,6 +1,12 @@
-class StorageService {
-  constructor() {
+const fs = require('fs');
 
+class StorageService {
+  constructor(folder) {
+    this._folder = folder;
+
+    if (!fs.existsSync(folder)) {
+      fs.mkdirSync(folder, { recursive: true });
+    }
   }
 }
 
