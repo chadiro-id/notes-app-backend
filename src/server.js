be433b66c1abf574ae00313b3d31c41a020fb37d
chadiro <196056109+chadiro-id@users.noreply.github.com>
2025-08-13 16:08:38 +0700

    Registering hapi/inert plugin

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
+const Inert = require('@hapi/inert');
 const path = require('path');
 
 const ClientError = require('./exceptions/ClientError');
@@ -53,6 +54,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
